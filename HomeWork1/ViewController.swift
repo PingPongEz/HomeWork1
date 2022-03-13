722727093f3b2b80ead2bec5b9ed4f68e53fda00
Sergey <kopgamerk@gmail.com>
2022-03-13 21:29:43 +0300

    )

--- HomeWork1/ViewController.swift
+++ HomeWork1/ViewController.swift
@@ -7,52 +7,63 @@
 
 import UIKit
 
+enum color {
+    case red
+    case yellow
+    case green
+}
+
 class ViewController: UIViewController {
     @IBOutlet var redLight: UIView!
     @IBOutlet var yellowLight: UIView!
     @IBOutlet var greenLight: UIView!
+    
     @IBOutlet var button: UIButton!
     
-    enum color {
-        case Red
-        case Yellow
-        case Green
-    }
+    private var currentColor = color.green
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.4
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
-        redLight.alpha = 0.4
-        redLight.layer.cornerRadius = redLight.layer.frame.height / 2
-        
-        yellowLight.alpha = 0.4
-        yellowLight.layer.cornerRadius = yellowLight.layer.frame.height / 2
-        
-        greenLight.alpha = 0.4
-        greenLight.layer.cornerRadius = greenLight.layer.frame.height / 2
+        redLight.alpha = lightIsOff
+        yellowLight.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
         
         button.setTitle("Press Me!", for: .normal)
         button.layer.cornerRadius = 10
+        
     }
     
-        var currentColor = color.Green
+    override func viewWillLayoutSubviews() {
+        
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+
+    }
     
     @IBAction func pressTheButton() {
          
+        if button.currentTitle?.lowercased() == "press me!" {
+            button.setTitle("Next", for: .normal)
+        }
+        
         switch currentColor {
-        case .Red:
-            yellowLight.alpha = 1
-            redLight.alpha = 0.4
-            currentColor = .Yellow
-        case .Yellow:
-            yellowLight.alpha = 0.4
-            greenLight.alpha = 1
-            currentColor = .Green
-        case .Green:
-            greenLight.alpha = 0.4
-            redLight.alpha = 1
-            currentColor = .Red
+        case .red:
+            redLight.alpha = lightIsOff
+            yellowLight.alpha = lightIsOn
+            currentColor = .yellow
+        case .yellow:
+            yellowLight.alpha = lightIsOff
+            greenLight.alpha = lightIsOn
+            currentColor = .green
+        case .green:
+            greenLight.alpha = lightIsOff
+            redLight.alpha = lightIsOn
+            currentColor = .red
         }
     }
     
